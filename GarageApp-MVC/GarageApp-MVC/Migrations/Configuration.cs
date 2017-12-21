13440c621564cd3ca36cad8eed130f2666466c90
swapna <pasupuletisaiswapna85@gmail.com>
2017-12-21 13:26:48 +0100

    finalpartdone

--- GarageApp-MVC/GarageApp-MVC/Migrations/Configuration.cs
+++ GarageApp-MVC/GarageApp-MVC/Migrations/Configuration.cs
@@ -5,6 +5,7 @@ namespace GarageApp_MVC.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+  
 
     internal sealed class Configuration : DbMigrationsConfiguration<GarageApp_MVC.DataAccessLayer.RegisterContext>
     {
@@ -15,6 +16,10 @@ namespace GarageApp_MVC.Migrations
 
         protected override void Seed(GarageApp_MVC.DataAccessLayer.RegisterContext context)
         {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
             context.Vehicletypes.AddOrUpdate(v => v.VType,
                  new VehicleType() { VType = "Auto" },
                  new VehicleType() { VType = "Bus" },
@@ -22,6 +27,64 @@ namespace GarageApp_MVC.Migrations
                  new VehicleType() { VType = "Car" },
                  new VehicleType() { VType = "Truck" }
                  );
+
+            context.SaveChanges();
+
+            context.Members.AddOrUpdate(v => v.mobile,
+                new Member() { OwnerName = "Scott S", email = "sc_t@gmail.com", mobile = "0721987432" },
+                new Member() { OwnerName = "Nile G", email = "Nile@hotmail.com", mobile = "072109001" });
+
+
+
+            context.SaveChanges();
+
+            int i = 1;
+            foreach (Models.Member c in context.Members)
+            {
+
+                context.Vehicles.AddOrUpdate(v => v.RegNum,
+                   new ParkedVehicle()
+                   {
+
+                       RegNum = "AQR101",
+                       Color = "Orange",
+                       Brand = "Hyonda",
+                       Model = "Aws",
+                       NoOfWheels = 3,
+                       SeatCapacity = 4,
+                       ParkingTime = DateTime.Now.AddMinutes(10),
+                       MemberId = c.Id,
+                     
+                       VehicleType = context.Vehicletypes.ToList()[i++]
+                   },
+
+
+                new ParkedVehicle()
+                {
+                    RegNum = "AMN202",
+                    Color = "Orange",
+                    Brand = "Hyonda",
+                    Model = "Aws",
+                    NoOfWheels = 3,
+                    SeatCapacity = 4,
+                    ParkingTime = DateTime.Now.AddMinutes(10),
+                    MemberId = c.Id,
+                    VehicleType = context.Vehicletypes.ToList()[i++]
+                }
+
+              );
+
+
+
+
+            }
+            context.SaveChanges();
+
+
+
+
+
+
         }
     }
 }
